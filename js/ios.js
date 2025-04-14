31c3296fd94a69128c17fbea6807d55a84d44ce0
Ruan <ruanghrmacedo2014@gmail.com>
2025-04-14 14:30:59 -0300

    error update

--- js/ios.js
+++ js/ios.js
@@ -590,7 +590,7 @@ $(document).ready(function () {
                             error: function (xhr, status, error) {
                                 console.error('❌ Erro ao enviar: ' + error);
 
-                                if (xhr.status === 500){
+                                if (xhr.status === 500 && error === 500){
                                     alert('Este número de telefone já foi cadastrado!');
                                 } else {
                                     alert('Ocorreu um erro ao enviar os dados. Tente novamente mais tarde.');
